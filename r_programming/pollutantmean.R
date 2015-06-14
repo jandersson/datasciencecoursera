448007ae4d1a6dca14febc2ea96d596cbf1e25f5
Jonas Andersson <jonas@andersson.io>
2015-06-14 12:47:45 -0500

    Compute average of given pollutant across all station monitors

--- r_programming/pollutantmean.R
+++ r_programming/pollutantmean.R
@@ -1,8 +1,8 @@
 convert_to_station_file <- function(number){
   # This function will prepend or pad 0's upon a given number
   if (number < 10){station_id <- paste0("00", number)}
-  if (number < 100 && number > 9){station_id <- paste0("0", number)}
-  if (number < 1000 && number > 99){station_id <- number}
+  else if (number < 100 && number > 9){station_id <- paste0("0", number)}
+  else if (number < 1000 && number > 99){station_id <- number}
   file_name <- paste(station_id,".csv", sep="")
   file_name
 }
@@ -12,17 +12,27 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## the location of the CSV files
   old.dir <- getwd()
   setwd(directory)
-  mean_vec <- vector("numeric", length=length(id))
+  #Create empty vectors with a size equal to the length of the id vector
+  points_vec <- vector("numeric", length=length(id))
+  sum_vec <- vector("numeric", length=length(id))
+  id_len <- length(id)
+  i <- 1
   for(single_id in id){
-    file_name <- convert_to_station_file(id[single_id])
-    print(file_name)
+    file_name <- convert_to_station_file(single_id)
     if(file.exists(file_name)){data <- read.csv(file_name)}
     if(pollutant == "sulfate"){
       nas <- is.na(data$sulfate)
       sulfate <- data$sulfate[!nas]
-      avg_sulfate <- mean(sulfate)
-      mean_vec[single_id] <- avg_sulfate
+      sum_vec[i] <- sum(data$sulfate[!nas])
+      points_vec[i] <- length(sulfate)
     }
+    else if(pollutant == "nitrate"){
+      nas <- is.na(data$nitrate)
+      nitrate <- data$nitrate[!nas]
+      sum_vec[i] <- sum(data$nitrate[!nas])
+      points_vec[i] <- length(nitrate)
+    }
+    i <- i + 1
   }
   
   ## 'pollutant' is a character vector of length 1 indicating
@@ -50,5 +60,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
   setwd(old.dir)
-  mean(mean_vec)
+  points_sum <- sum(points_vec)
+  sum_of_sums <- sum(sum_vec)
+  sum_of_sums/points_sum
 }
